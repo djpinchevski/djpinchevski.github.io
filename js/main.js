ba8e1351008b118e8aef42fd9bce1b73fc82f82e
Dave Pinchevski <dave.pinchevski@vinciworks.com>
2021-04-03 15:59:34 +0300

    Add names to meet_the_team; Shorten scrollTrigger duration;

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 gsap.from("#how_can_we_help", {
   scrollTrigger: "#how_can_we_help .helpful_items", 
   x: window.innerWidth,
-  duration: 1.5,
+  duration: 0.75
 });
 
 gsap.from("#what_do_our_clients_say", {
   scrollTrigger: "#what_do_our_clients_say .client_container", 
   x: window.innerWidth * -1,
-  duration: 1.5,
+  duration: 0.75
 });
 
 gsap.from("#our_story", {
   scrollTrigger: "#our_story p", 
   x: window.innerWidth,
-  duration: 1.5,
+  duration: 0.75
 });
 
 gsap.from("#meet_the_team", {
   scrollTrigger: "#meet_the_team .grid", 
   x: window.innerWidth * -1,
-  duration: 1.5,
+  duration: 0.75
 });
